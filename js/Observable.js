dc9de25e09448bce3f4b904eafe5316edf691f69
Jakub Jankiewicz <jcubic@onet.pl>
2020-03-14 23:22:00 +0100

    add info comment to Observable

--- js/Observable.js
+++ js/Observable.js
@@ -1,3 +1,5 @@
+// ES Next Observable code taken from proposal repo and run through babel
+// (+ removed exports)
 "use strict";
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
